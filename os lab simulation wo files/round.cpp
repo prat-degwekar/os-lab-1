03ba7a1d251fc425603e26cf7efd4cc93ff6b197
prat-degwekar <prat.degwekar@gmail.com>
2017-08-22 16:22:58 +0530

    round robin updated

--- os lab simulation wo files/round.cpp
+++ os lab simulation wo files/round.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "proceed.h"
+#include <queue.h>
 
 void rr( process p[] , int n )
 {
@@ -15,43 +16,57 @@ void rr( process p[] , int n )
 	int j = 0;
 	int flg = 0;
 	int flg2 = 0;
+	queue<int> q;
+	int vis[n];
+
+	//q.enqueue(0);
+
+	for(int i=0 ; i<n ; i++)
+		vis[i] = 0;
 
 	cout<<"i\tcurslic\tnextsli\tj\tpid\tbt\n";
 
 	for(int i=0 ; flg2 == 0 ; )
 	{
-		if( p[j].getat() <= curt && p[j].getct() < 0 )
+		for(j=0 ; j<n ; j++)
 		{
-			if(p[j].getbt() < cur_slice)
+			if( p[j].getat() <= curt && p[j].getct() < 0 )
 			{
-				next_slice+=cur_slice - p[j].getbt();
-			}
+				/*if(p[j].getbt() < cur_slice)
+				{
+					next_slice+=cur_slice - p[j].getbt();
+				}*/
 	
-			p[j].setbt( p[j].getbt() - cur_slice );
+				//p[j].setbt( p[j].getbt() - cur_slice );
 	
-			if(p[j].getbt() <= 0)
-			{
-				p[j].setct( curt + next_slice - 3 + cur_slice );
-				p[j].caltat();
-				p[j].calwt();
-				p[j].setbt(0);
+				/*if(p[j].getbt() <= 0)
+				{
+					p[j].setct( curt + next_slice - 3 + cur_slice );
+					p[j].caltat();
+					p[j].calwt();
+					p[j].setbt(0);
+				}*/
+				q.enqueue(j);
+				flg = 1;
 			}
-			flg = 1;
 		}
 
-		if(flg == 0)
+		/*if(flg == 0)
 		{
-			j = (j + 1) % n;
+			//j = (j + 1) % n;
 			continue;
-		}
-
+		}*/
+		
+		//enqueueing part is done , now to execute
+	
+		//dequeue , run a slice , check if bt < 0 -> enqueue again. finally one dequeue and enqueue w/o run process.
 		i+=cur_slice;
 		curt+=cur_slice;
 		cur_slice = next_slice;
 		next_slice = 3;
 		flg = 0;
 
-		cout<<i<<"\t"<<cur_slice<<"\t"<<next_slice<<"\t"<<j<<"\t"<<p[j].getpid()<<"\t"<<p[j].getbt()<<"\t"<<endl;
+		/*cout<<i<<"\t"<<cur_slice<<"\t"<<next_slice<<"\t"<<j<<"\t"<<p[j].getpid()<<"\t"<<p[j].getbt()<<"\t"<<endl;
 		j = ( j + 1 ) % n;
 
 		for(int jj = 0 ; jj < n ; jj++)
@@ -67,5 +82,7 @@ void rr( process p[] , int n )
 			flg2 = 0;
 		else
 			break;
+
+		*/
 	}
 }
